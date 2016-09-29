5b3666f0a0607958d04c51aa792ba5db25737562
Hirokilen <dylanraimon@gmail.com>
2016-09-29 10:35:55 +0200

    Meh

---
+++ ReaderObject/Employe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderObject
+{
+    class Employe
+    {
+        public Int16 Numemp { get; set; }
+        public String Nomemp { get; set; }
+        public String Prenomemp { get; set; }
+        public String Poste { get; set; }
+        public String Salaire { get; set; }
+        public Single? Prime { get; set; } // nullable
+        public String CodeProjet { get; set; }
+        public Int16? Superieur { get; set; } // nullable
+
+        public override string ToString()
+        {
+            return Numemp + " . " + Nomemp + " . " + Prenomemp + " . " + Poste + " . " + Salaire + " . " + Prime + " . " + CodeProjet + " . " + Superieur;
+        }
+    }
+
+}
---
+++ ReaderObject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+
+namespace ReaderObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OracleConnection CnOracle;
+            String Cn = "Data Source=raimonsql;User Id=raimonsql; Password=sio";
+            CnOracle = new OracleConnection();
+            CnOracle.ConnectionString = Cn;
+            CnOracle.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            string cmdEmp = " select * from employe";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                        reader.GetString(1));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+        
+
+        }
+    }
+}
